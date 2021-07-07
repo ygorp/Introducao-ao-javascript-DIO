1e62588826f80c80e36e9b2debb6251c5e4c2372
ygorp <ygor-nogueira@hotmail.com>
2021-07-07 17:44:01 -0300

    subindo

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+var nome = "Ygor Pacheco"
+var idade = 29;
+var frase = "Brasil, o melhor pais do mundo"
+//alert(nome + "tem" + idade + "anos");
+
+console.log(nome);
+console.log(idade);
+console.log(frase);
+
+
